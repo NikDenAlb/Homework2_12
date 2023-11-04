b9fbf3d74fdbb7220c5b9d8c1caca903385f3780
NuzGooL <ardeniaanderland@gmail.com>
2023-11-04 05:24:47 +0300

    Initial commit

---
+++ src/test/java/com/example/Homework2_12/CalcForTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Homework2_12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcForTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
