df2ef09f9fbe2c0c867c5aab1cd4c01dead8bfbc
NuzGooL <ardeniaanderland@gmail.com>
2023-11-05 23:39:28 +0300

    1.00

---
+++ src/main/java/com/example/Homework2_12/CannotDivideByZeroExeption.java
@@ -0,0 +1,7 @@
+package com.example.Homework2_12;
+
+class CannotDivideByZeroExeption extends IllegalArgumentException{
+    public CannotDivideByZeroExeption(String message) {
+        super(message);
+    }
+}
